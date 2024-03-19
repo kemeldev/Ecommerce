73486aa4747e6af1102e3bac8accad1220c356a5
kemeldev <kemel.developer@gmail.com>
2024-03-18 18:51:29 -0600

    Updates

--- src/constants/index.js
+++
@@ -1,7 +0,0 @@
-export const brands = ['adidas', 'under armour', 'converse', 'nike']
-
-export const categories = ['running', 'football', 'training', 'outdoor']
-
-export const promotions = ['Products with discount']
-
-export const sizes = [7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13]
